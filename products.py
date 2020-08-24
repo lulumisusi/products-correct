01b610d8dcc9c2716b32c701ae7d68ae49d0c70d
linlinchang <lulumisusi@gmail.com>
2020-08-24 14:22:06 +0800

    add write

--- products.py
+++ products.py
@@ -1,10 +1,20 @@
-# 輸入商品及價錢
+# 讀取檔案
 products = []
+with open('products.csv', 'r' , encoding ='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+        products.append([name, price])
+print(products)
+
+# 輸入商品及價錢
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')	
+	price = int(price)
 	products.append([name, price])
 print(products)
 
@@ -12,7 +22,7 @@ for p in products:
 	print(p[0], '的價格', p[1])
 
 # 打開檔案
-with open('products.csv','w') as f:
+with open('products.csv','w' , encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
